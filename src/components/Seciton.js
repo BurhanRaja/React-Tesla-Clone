6386cb9bc2de4f027f4bde587511bd62e520305c
BurhanuddinRaja <mails4burhan@gmail.com>
2022-07-04 22:15:27 +0530

    Down arrow added

--- src/components/Seciton.js
+++ src/components/Seciton.js
@@ -4,7 +4,21 @@ import styled from 'styled-components'
 const Seciton = () => {
     return (
         <Wrap>
-            
+            <ItemText>
+                <h1>Model 3</h1>
+                <br/>
+                <p>Order Online for Touchless Delivery</p>
+            </ItemText>
+
+                <ButtonGroup>
+                    <LeftButton>
+                        Custom Order
+                    </LeftButton>
+                    <RightButton>
+                        Existing Inventory
+                    </RightButton>
+                </ButtonGroup>
+                <DownArrow src="/images/down-arrow.svg" />
         </Wrap>
     )
 }
@@ -12,8 +26,55 @@ const Seciton = () => {
 export default Seciton
 
 const Wrap = styled.div({
-    width:"100vw",
-    height:"100vh",
-    backgroundSize:"cover",
-    backgroundImage:"url(/images/model-s.jpg)"
+    width: "100vw",
+    height: "100vh",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundImage: "url(/images/model-s.jpg)"
 })
+
+const ItemText = styled.div({
+    padding: "13vh",
+})
+
+const ButtonGroup = styled.div({
+    marginTop: "42vh"
+})
+const LeftButton = styled.button({
+    backgroundColor: "#2a2a2a",
+    color: "white",
+    padding: "12px 80px",
+    textTransform: "uppercase",
+    borderRadius: "100px",
+    fontSize: "12px",
+    fontWeight: "bold",
+    border: "none",
+    opacity: "0.85",
+    margin: "7px 7px",
+    cursor: "pointer"
+})
+const RightButton = styled.button({
+    backgroundColor: "#f9f8f8",
+    color: "black",
+    padding: "12px 70px",
+    textTransform: "uppercase",
+    borderRadius: "100px",
+    fontSize: "12px",
+    fontWeight: "bold",
+    border: "none",
+    opacity: "0.85",
+    margin: "7px 7px",
+    cursor: "pointer"
+})
+
+const DownArrow = styled.img({
+    marginTop: "4vh",
+    height:"40px",
+    animation:"animateDown infinite 1.5s",
+    overflowX:"hidden"
+})
+
+// const Buttons = styled.button({
+//     // margin: "2px"
+// })
